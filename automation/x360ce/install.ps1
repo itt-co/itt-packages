b4e2f27997212075034046b979d8605c3b7d4bf4
ITT Community <167635892+itt-co@users.noreply.github.com>
2025-02-25 02:17:29 +0200

    Update install.ps1

--- automation/x360ce/install.ps1
+++ automation/x360ce/install.ps1
@@ -1,13 +1,14 @@
 $ErrorActionPreference = 'Stop'
 
 $packageArgs = @{
-    packageName    = 'x360ce'
-    fileType       = 'zip'
-    url            = 'https://www.x360ce.com/files/x360ce_x86.zip'
-    url64          = 'https://www.x360ce.com/files/x360ce_x64.zip'
-    installerName  = 'x360ce.exe'
-    silentArgs     = 'na'
-    validExitCodes = @(0, 1223)
+    packageName     = 'x360ce'
+    fileType        = 'zip'
+    url             = 'https://www.x360ce.com/files/x360ce_x86.zip'
+    url64           = 'https://www.x360ce.com/files/x360ce_x64.zip'
+    installerName   = 'x360ce_x32.exe'
+    installerName64 = 'x360ce_x64.exe'
+    silentArgs      = 'na'
+    validExitCodes  = @(0, 1223)
 }
 
 Install-ITTPackage @packageArgs
