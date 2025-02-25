7b3df92426ae95282d9bcf071b5fc9b80cb1c9f4
Emad Adel <167635892+itt-co@users.noreply.github.com>
2025-02-25 11:22:31 +0200

    Create install.ps1

---
+++ automation/selfishnet/install.ps1
@@ -0,0 +1,14 @@
+$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+    packageName    = 'selfishnet'
+    fileType       = 'zip'
+    url            = 'https://github.com/nov0caina/SelfishNetV3/releases/download/SelfishNetV3.0.0/SelfishNetV3.0.0_Installer.zip'
+    url64          = 'https://github.com/nov0caina/SelfishNetV3/releases/download/SelfishNetV3.0.0/SelfishNetV3.0.0_Installer.zip'
+    installerName  = 'SelfishNetV3_Installer.msi'
+    installerName64 = 'SelfishNetV3_Installer.msi'
+    silentArgs     = '/quiet'
+    validExitCodes = @(0, 1223)
+}
+
+Install-ITTPackage @packageArgs
