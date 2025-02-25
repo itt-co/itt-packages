4dd72fda02b1628c8e95532366bbf592dc84cb83
Emad Adel <167635892+itt-co@users.noreply.github.com>
2025-02-25 11:19:10 +0200

    Create install.ps1

---
+++ automation/aimp/install.ps1
@@ -0,0 +1,14 @@
+$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+    packageName    = 'aimp'
+    fileType       = 'exe'
+    url            = 'https://aimp.ru/files/windows/builds/aimp_5.40.2655_w32.exe'
+    url64          = 'https://aimp.ru/files/windows/builds/aimp_5.40.2655_w64.exe'
+    installerName  = 'aimp_5.40.2655_w32.exe'
+    installerName64 = 'aimp_5.40.2655_w64.exe'
+    silentArgs     = '/AUTO /SILENT /RUN'
+    validExitCodes = @(0, 1223)
+}
+
+Install-ITTPackage @packageArgs
