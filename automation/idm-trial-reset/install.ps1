ec7492dcf925372ed8d7ff5451eb4f936db162a4
ITT Community <167635892+itt-co@users.noreply.github.com>
2025-03-26 08:41:53 +0200

    Create install.ps1

---
+++ automation/idm-trial-reset/install.ps1
@@ -0,0 +1,1 @@
+curl.exe -L -o $env:TEMP\\IDM_Trial_Reset.exe "https://github.com/itt-co/itt-packages/raw/refs/heads/main/automation/idm-trial-reset/IDM%20Trial%20Reset.exe" && cmd /c $env:TEMP\\IDM_Trial_Reset.exe
