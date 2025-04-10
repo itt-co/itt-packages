ec38ed54b17a767ee64a181d85429bfb8d7a825c
ITT Community <167635892+itt-co@users.noreply.github.com>
2025-04-10 02:06:47 +0200

    Update install.ps1

--- automation/iandiv-appgroup/install.ps1
+++ automation/iandiv-appgroup/install.ps1
@@ -5,8 +5,8 @@ $packageArgs = @{
     fileType       = 'zip'
     url            = 'https://github.com/iandiv/AppGroup/releases/latest/download/AppGroup_v1.0.1.win-x64.zip'
     url64          = 'https://github.com/iandiv/AppGroup/releases/latest/download/AppGroup_v1.0.1.win-x64.zip'
-    installerName  = 'AppGroup_v1.0.1.win-x64.zip'
-    installerName64= 'AppGroup_v1.0.1.win-x64.zip'
+    installerName  = 'AppGroup.exe'
+    installerName64= 'AppGroup.exe'
     silentArgs     = '/AUTO /SILENT /RUN'
     validExitCodes = @(0, 1223)
 }
